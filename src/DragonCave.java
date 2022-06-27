3556ac375b7f559481ce84b83ba940c5a6b05158
Elijah-Piper <piperelijah@gmail.com>
2022-06-27 09:38:24 -0700

    Changed dialogue variable from concatenated strings to text blocks.

--- src/DragonCave.java
+++ src/DragonCave.java
@@ -7,7 +7,7 @@ public class DragonCave {
         // Determination of which cave will contain the hungry dragon and which cave will contain the friendly dragon.
         // Gives a random integer in range 1 to (non-inclusive) 3 (meaning options are either 1 or 2.
         int hungryCave = ThreadLocalRandom.current().nextInt(1, 3);
-
+/*
         // The dialogue strings are the console printouts for the game story and player guidance.
         String dialogueInitial =
                 "You are in a land full of dragons. In front of you,\n" +
@@ -26,7 +26,30 @@ public class DragonCave {
                 "You see a large dragon sleeping,\n" +
                 "having gorged himself the local shepherd's goats!\n" +
                 "Your eyes catch a pile of treasure in the opposite corner...\n" +
-                "You collect all you can carry and escape without disturbing the beast!";
+                "You collect all you can carry and escape without disturbing the beast!"; */
+
+        // The dialogue strings are the console printouts for the game story and player guidance.
+        String dialogueInitial = """
+                You are in a land full of dragons. In front of you,
+                you see two caves. In one cave, the dragon is friendly
+                and will share his treasure with you. The other dragon
+                is greedy and hungry and will eat you on sight.
+                Which cave will you go into? (1 or 2)/n
+                """;
+        String dialogueHungry = """
+                \nYou approach the cave...
+                It is dark and spooky...
+                A large dragon jumps out in front of you! He opens his jaws and...
+                Gobbles you down in one bite!
+                """;
+        String dialogeFriendly = """
+                \nYou approach the cave...
+                The sun peers through a skylight...
+                You see a large dragon sleeping,
+                having gorged himself the local shepherd's goats!
+                Your eyes catch a pile of treasure in the opposite corner...
+                You collect all you can carry and escape without disturbing the beast!
+                """;
 
         // Scanner for console detected user input
         Scanner scan = new Scanner(System.in);
