db068434f467a56839ef019f5112abd4859ed44c
Elijah-Piper <piperelijah@gmail.com>
2022-06-27 09:40:38 -0700

    Removed redundant commented dialogue variable code.

--- src/DragonCave.java
+++ src/DragonCave.java
@@ -7,26 +7,6 @@ public class DragonCave {
         // Determination of which cave will contain the hungry dragon and which cave will contain the friendly dragon.
         // Gives a random integer in range 1 to (non-inclusive) 3 (meaning options are either 1 or 2.
         int hungryCave = ThreadLocalRandom.current().nextInt(1, 3);
-/*
-        // The dialogue strings are the console printouts for the game story and player guidance.
-        String dialogueInitial =
-                "You are in a land full of dragons. In front of you,\n" +
-                "you see two caves. In one cave, the dragon is friendly\n" +
-                "and will share his treasure with you. The other dragon\n" +
-                "is greedy and hungry and will eat you on sight.\n" +
-                "Which cave will you go into? (1 or 2)\n";
-        String dialogueHungry =
-                "\nYou approach the cave...\n" +
-                "It is dark and spooky...\n" +
-                "A large dragon jumps out in front of you! He opens his jaws and...\n" +
-                "Gobbles you down in one bite!";
-        String dialogeFriendly =
-                "\nYou approach the cave...\n" +
-                "The sun peers through a skylight...\n" +
-                "You see a large dragon sleeping,\n" +
-                "having gorged himself the local shepherd's goats!\n" +
-                "Your eyes catch a pile of treasure in the opposite corner...\n" +
-                "You collect all you can carry and escape without disturbing the beast!"; */
 
         // The dialogue strings are the console printouts for the game story and player guidance.
         String dialogueInitial = """
