94f2cef86ce28a405a2a178dfac440fe656e7c1e
Elijah-Piper <piperelijah@gmail.com>
2022-07-04 15:32:55 -0700

    Added testing template file for DragonCave class.

---
+++ src/test/java/DragonCaveTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DragonCaveTest {
+
+    @Test
+    void placeholder() {
+        assertTrue(true);
+    }
+}
